c376d66a41231724d06a4a3c1ee9bfc1f71771fb
SirusDoma <com@cxo2.me>
2018-02-12 12:44:02 +0700

    Updated example program to reflect API changes

--- Source/Example/Program.cs
+++ Source/Example/Program.cs
@@ -14,34 +14,17 @@ namespace Example
             // This is ensure our SoundSystem will be disposed when program about to exit
             using (var system = SoundSystem.Instance)
             {
-                // For simplicity sake, use Auto Update
-                system.AutoUpdate();
-
-                // Load and play the audio with Sound class
-                var sound = new Sound(new SoundBuffer("./Resources/canary.wav"));
-                system.Play(sound);
-
-                var length = sound.Duration;
-                Console.WriteLine("Playing: canary.wav");
-                Console.WriteLine("Implementation: Sound");
-                while (sound.Status == SoundStatus.Playing)
-                {
-                    var offset = sound.PlayingOffset;
-                    Console.Write("\r{0}:{1}:{2} - {3}:{4}:{5}",
-                        offset.Minutes.ToString("00"), offset.Seconds.ToString("00"), offset.Milliseconds.ToString("000"),
-                        length.Minutes.ToString("00"), length.Seconds.ToString("00"), length.Milliseconds.ToString("000")
-                    );
-                }
-
                 // Now with music
                 var music = new Music("./Resources/orchestral.ogg");
+                //music.IsLooping = true;
                 system.Play(music);
 
-                length = music.Duration;
-                Console.WriteLine("\n\nPlaying: ochestral");
+                var length = music.Duration;
+                Console.WriteLine("Playing: ochestral");
                 Console.WriteLine("Implementation: Music");
                 while (music.Status == SoundStatus.Playing)
                 {
+                    system.Update();
                     var offset = music.PlayingOffset;
                     Console.Write("\r{0}:{1}:{2} - {3}:{4}:{5}",
                         offset.Minutes.ToString("00"), offset.Seconds.ToString("00"), offset.Milliseconds.ToString("000"),
@@ -49,7 +32,25 @@ namespace Example
                     );
                 }
 
+                // Load and play the audio with Sound class
+                var sound = new Sound(new SoundBuffer("./Resources/canary.wav"));
+                system.Play(sound);
 
+                length = sound.Duration;
+                Console.WriteLine("\n\nPlaying: canary.wav");
+                Console.WriteLine("Implementation: Sound");
+                while (sound.Status == SoundStatus.Playing)
+                {
+                    system.Update();
+                    var offset = sound.PlayingOffset;
+                    Console.Write("\r{0}:{1}:{2} - {3}:{4}:{5}",
+                        offset.Minutes.ToString("00"), offset.Seconds.ToString("00"), offset.Milliseconds.ToString("000"),
+                        length.Minutes.ToString("00"), length.Seconds.ToString("00"), length.Milliseconds.ToString("000")
+                    );
+                }
+
+
+                Console.CursorVisible = false;
                 Console.WriteLine("\n\nPress any key to exit program.");
                 Console.ReadKey(true);
             }
